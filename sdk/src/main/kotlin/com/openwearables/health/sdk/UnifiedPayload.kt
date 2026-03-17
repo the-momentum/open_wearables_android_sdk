a49beead34cd6d3a73d19323596de91a99fb04e2
kmlpiekarz <kamil.piekarz@applover.pl>
2026-03-17 11:54:45 +0100

    add zone offset handling based on record time zone

--- sdk/src/main/kotlin/com/openwearables/health/sdk/UnifiedPayload.kt
+++ sdk/src/main/kotlin/com/openwearables/health/sdk/UnifiedPayload.kt
@@ -1,7 +1,6 @@
 package com.openwearables.health.sdk
 
 import java.time.Instant
-import java.time.ZoneId
 import java.time.ZoneOffset
 import java.time.format.DateTimeFormatter
 
@@ -183,10 +182,6 @@ object UnifiedTimestamp {
     fun fromEpochMs(epochMs: Long): String =
         isoFormatter.format(Instant.ofEpochMilli(epochMs))
 
-    fun zoneOffsetString(): String {
-        val offset = ZoneId.systemDefault().rules.getOffset(Instant.now())
-        return if (offset.totalSeconds == 0) "+00:00" else offset.toString()
-    }
 }
 
 // ---------------------------------------------------------------------------
